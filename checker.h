5c38e25451039516b34d346fef804ac829cedc8d
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-02-08 19:07:38 +0530

    Delete checker.h

--- checker.h
+++
@@ -1,6 +0,0 @@
-#define TEMP_MIN            0
-#define TEMP_MAX            45
-#define SOC_MIN             20
-#define SOC_MAX             80
-#define CHARGERATE_MIN      0.0
-#define CHARGERATE_MAX      0.8
