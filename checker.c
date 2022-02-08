57809d2a3aa83ddd45b7c89baf56f6db5030c630
SujnaR-Bhat <91068079+SujnaR-Bhat@users.noreply.github.com>
2022-02-08 19:07:02 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,6 +2,13 @@
 #include <assert.h>
 #include "checker.h"
 
+/* MACROS */
+#define TEMP_MIN            0
+#define TEMP_MAX            45
+#define SOC_MIN             20
+#define SOC_MAX             80
+#define CHARGERATE_MIN      0.0
+#define CHARGERATE_MAX      0.8
 
 
 int temperature_range (float temperature)
